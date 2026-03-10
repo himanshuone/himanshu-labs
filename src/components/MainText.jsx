cd888c63d44898d765c38cad4d00d7b5757a41e2
Himanshu Kumar <79431391+himanshuone@users.noreply.github.com>
2026-03-11 00:04:10 +0530

    BAckground done

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -3,9 +3,10 @@ function MainText(){
     return (
         <>
         
-        <div>
-            PORTFOLIO
-        </div>
+        
+        <h1 class="font-roboto text-[4rem] text-[#5c4f2e] select-none [text-shadow:2px_2px_0_#fff,4px_4px_0_rgba(0,0,0,0.1)]">
+PORTFOLIO
+</h1>
         </>
     )
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   theme: {
+    extend: {
+      textShadow: {
+        sm: '1px 1px 2px rgba(0,0,0,0.3)',
+        md: '2px 2px 4px rgba(0,0,0,0.4)',
+        lg: '4px 4px 8px rgba(0,0,0,0.5)',
+      },
+      fontFamily: {
+        roboto: ['"Roboto Flex"', 'sans-serif'],
+      },
+    },
+  },
   plugins: [tailwindcss(),
+     require('tailwindcss-textshadow'),
+
   react()],
 })
+
