342573742bee053bfa765ad2a9ea37278f510781
Himanshu Kumar <79431391+himanshuone@users.noreply.github.com>
2026-03-16 01:15:02 +0530

    some text effectt

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-   theme: {
-    extend: {
-       fontFamily: {
-      roboto: ['Roboto Flex', 'sans-serif'],
-      nunito: ["Nunito", "sans-serif"],
-      lato: ['Lato', 'sans-serif'],
-    },
-      textShadow: {
-        sm: '1px 1px 2px rgba(0,0,0,0.3)',
-        md: '2px 2px 4px rgba(0,0,0,0.4)',
-        lg: '4px 4px 8px rgba(0,0,0,0.5)',
-      },
-    
-    },
-  },
-  plugins: [tailwindcss(),
-    
-
-  react()],
-})
-
+  plugins: [react(), tailwindcss()],
+});
